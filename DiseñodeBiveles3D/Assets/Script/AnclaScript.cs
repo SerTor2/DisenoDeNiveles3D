cdeebd98bba03ebf828b4971492621d775d1900f
SerTor2 <sertor27@gmail.com>
2019-05-15 20:35:28 +0200

    cambios

---
+++ DiseñodeBiveles3D/Assets/Script/AnclaScript.cs
@@ -0,0 +1,75 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AnclaScript : MonoBehaviour
+{
+    public Transform posToGo;
+    private Vector3 startPosition;
+    private float currentTime = 0;
+    private bool go = true;
+    private Vector3 differencePosition = new Vector3();
+    private Vector3 lastPosition = new Vector3();
+    private PlayerScript player;
+    // Start is called before the first frame update
+    void Start()
+    {
+        startPosition = gameObject.transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        lastPosition = transform.position;
+        if (go)
+        {
+            currentTime += Time.deltaTime;
+            if(currentTime >= 1)
+            {
+                gameObject.transform.position = Vector3.MoveTowards(gameObject.transform.position, posToGo.position, 50 * Time.deltaTime);
+                if((gameObject.transform.position - posToGo.position).magnitude <= 0.5)
+                {
+                    go = false;
+                    currentTime = 0;
+                }
+
+            }
+        }
+        else
+        {
+            currentTime += Time.deltaTime;
+            if (currentTime >= 1.5)
+            {
+                gameObject.transform.position = Vector3.MoveTowards(gameObject.transform.position, startPosition, 10 * Time.deltaTime);
+                differencePosition = transform.position - lastPosition;
+                if (player != null)
+                {
+                    player.MovePlayerWithAncla(differencePosition);
+                }
+                if ((gameObject.transform.position - startPosition).magnitude <= 0.5)
+                {
+                    go = true;
+                    if (player != null)
+                    {
+                        player.gameObject.transform.parent = null;
+                        player = null;
+                    }
+                    currentTime = 0;
+                }
+
+            }
+        }
+        
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.gameObject.tag == "Player")
+        print("matar");
+    }
+
+    public void SetPlayer(PlayerScript _player)
+    {
+        player = _player;
+    }
+}
